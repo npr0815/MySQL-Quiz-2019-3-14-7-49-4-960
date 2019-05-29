b4aa56d7cbe505813a523a8f256c7aa46741bb87
nn <npr_0815@163.com>
2019-05-29 20:35:11 +0800

    finished practice2

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,3 @@
-# 取得每个company中最高薪水的人员名字
+﻿# 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+select m.name, c.companyName from company c join (select companyId, name, max(salary) as maxs from employee group by companyId) m on c.id = m.companyId;
