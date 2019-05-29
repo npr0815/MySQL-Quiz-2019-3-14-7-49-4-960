4adde8a6ce4076a7d0ad1dafc4481ea9839c1086
nn <npr_0815@163.com>
2019-05-29 20:35:30 +0800

    finished practice3

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,5 @@
-#取得平均薪水最高的公司
+﻿#取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
+select a.avgs, c.companyName from company c join (select companyId, avg(salary) as avgs from employee group by companyId) a on a.companyId=c.id;
 
 
